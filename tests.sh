7010ec4b419a8c02fe3615f3fa509988bbb9ea80
Bryleen <bryleen1@live.co.uk>
2022-05-11 13:27:17 +0000

    Edited files for my jenkins

--- tests.sh
+++ tests.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-python3 -m venv venv
-ls -l
-source venv/bin/activate
 declare -a directories=("front_end_api" "dare_api" "truth_api" "merge_api")
 for dir in "${directories[@]}"
 do
-    cd ${dir}
-    pip3 install -r requirements.txt
-    python3 -m pytest --cov=application
-    cd ..
+  cd ${dir}
+  sudo apt-get update
+  sudo apt-get install python3 python3-pip python3-venv
+  python3 -m venv venv
+  source venv/bin/activate
+  pip3 install -r test_requirements.txt
+  python3 -m pytest --cov=application
+  deactivate
+  cd ..
 done
